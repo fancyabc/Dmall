63c4f1b6a100d89bec3a52a741ae837813074ccc
fan <2310403052@qq.com>
2022-11-03 12:48:55 -0700

    在总路由中添加自定义路由转换器,正则表达式匹配用户名

--- Dmall/apps/user/urls.py
+++ Dmall/apps/user/urls.py
@@ -3,5 +3,5 @@ from .views import UsernameCountView
 
 urlpatterns = [
     # 判断用户名是否重复
-    path('usernames/<username>/count/', UsernameCountView.as_view()),
+    path('usernames/<username:username>/count/', UsernameCountView.as_view()),
 ]
