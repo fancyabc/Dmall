9520ab89210810005622f01bf1f11efb68da1379
fan <2310403052@qq.com>
2022-11-03 06:23:05 -0700

    配置 mysql 和 redis

--- Dmall/Dmall/settings.py
+++ Dmall/Dmall/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Dmall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',   # 数据库主机
+        'PORT': 3306,          # 数据库端口
+        'USER': 'fan',         # 数据库用户名
+        'PASSWORD': 'mypass',  # 数据库用户密码
+        'NAME':  'mall',
     }
 }
 
@@ -121,3 +125,25 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# et caches use redis
+
+CACHES = {
+    "default": { # 默认
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+    "session": { # session
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+}
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "session"
