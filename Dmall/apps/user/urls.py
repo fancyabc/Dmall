42be89ea1c5d398b700f8e7824438772ae8936e0
fan <2310403052@qq.com>
2022-11-05 05:15:49 -0700

    用户名登录接口

--- Dmall/apps/user/urls.py
+++ Dmall/apps/user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UsernameCountView, MobileCountView, RegisterView
+from .views import UsernameCountView, MobileCountView, RegisterView, LoginView
 
 urlpatterns = [
     # 判断用户名是否重复
     path('usernames/<username:username>/count/', UsernameCountView.as_view()),
     path('mobiles/<mobile:mobile>/count/', MobileCountView.as_view()),
     path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view()),
 ]
--- Dmall/apps/user/views.py
+++ Dmall/apps/user/views.py
@@ -46,6 +46,7 @@ class RegisterView(View):
         password2 = body_dict.get('password2')
         mobile = body_dict.get('mobile')
         allow = body_dict.get('allow')
+        sms_code = body_dict.get('sms_code')    # 短信验证码
 
         # 3. 验证数据
         #     3.1 用户名，密码，确认密码，手机号，是否同意协议 都要有
@@ -58,9 +59,21 @@ class RegisterView(View):
         if not re.match('[a-zA-Z_-]{5,20}', username):
             return JsonResponse({'code': 400, 'errmsg': '用户名不满足规则'})
         #     3.3 密码满足规则
+        # 判断密码是否是8-20个字符
+        if not re.match(r'^[0-9A-Za-z]{8,20}$', password):
+            return JsonResponse({'code': 400, 'errmsg': 'password格式有误!'})
+
         #     3.4 确认密码和密码要一致
+        if password != password2:
+            return JsonResponse({'code': 400, 'errmsg': '两次输入不对!'})
         #     3.5 手机号满足规则，手机号也不能重复
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
+            return JsonResponse({'code': 400, 'errmsg': 'mobile格式有误!'})
         #     3.6 需要同意协议
+        if not allow:
+            return JsonResponse({'code': 400, 'errmsg': 'allow格式有误!'})
+        #     3.7 短信验证码
+
         # 4. 数据入库
         # user=User(username=username,password=password,mobile=mobile)
         # user.save()
@@ -78,4 +91,54 @@ class RegisterView(View):
         login(request, user)
 
         # 5. 返回响应
+        return JsonResponse({'code': 0, 'errmsg': 'ok'})
+
+
+class LoginView(View):
+
+    def post(self, request):
+        # 1. 接收数据
+        data = json.loads(request.body.decode())
+        username = data.get('username')
+        password = data.get('password')
+        remembered = data.get('remembered')
+        # 2. 验证数据
+        if not all([username, password]):
+            return JsonResponse({'code': 400, 'errmsg': '参数不全'})
+
+        # 根据手机号查询 还是 根据用户名查询 ?
+        # USERNAME_FIELD 我们可以根据 修改 User. USERNAME_FIELD 字段
+        # 来影响authenticate 的查询
+        # authenticate 就是根据 USERNAME_FIELD 来查询
+        if re.match('1[3-9]\d{9}', username):
+            User.USERNAME_FIELD = 'mobile'
+        else:
+            User.USERNAME_FIELD = 'username'
+
+        # 3. 验证用户名和密码是否正确
+        # 方式1 我们可以通过模型根据用户名来查询
+        # User.objects.get(username=username)
+        # 方式2
+        from django.contrib.auth import authenticate
+        # authenticate 传递用户名和密码
+        # 如果用户名和密码正确，则返回 User信息
+        # 如果用户名和密码不正确，则返回 None
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            return JsonResponse({'code': 400, 'errmsg': '账号或密码错误'})
+
+        # 4. session
+        from django.contrib.auth import login
+        login(request, user)
+        # 5. 判断是否记住登录
+        if remembered:
+            # 记住登录 具体多长时间 产品说了算
+            request.session.set_expiry(None)
+
+        else:
+            # 不记住登录  浏览器关闭 session过期
+            request.session.set_expiry(0)
+
+        # 6. 返回响应
         return JsonResponse({'code': 0, 'errmsg': 'ok'})
