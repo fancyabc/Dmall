2e1f601f422b2d88b82487b2ffa1c3b57ec08613
fan <2310403052@qq.com>
2022-11-07 20:19:36 -0800

    后台准备fastdfs的docker镜像并启动，安装FastDFS客户端扩展并修改配置文件，进行图片上传测试

--- Dmall/apps/goods/views.py
+++ Dmall/apps/goods/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from fdfs_client.client import Fdfs_client
+
+# 1. 创建客户端
+# 修改加载配置文件的路径
+client=Fdfs_client('utils/fastdfs/client.conf')
+#
+# # 2. 上传图片
+# # 图片的绝对路径
+client.upload_by_filename('/home/fan/Desktop/img/c.png')
