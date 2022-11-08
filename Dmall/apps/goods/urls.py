2eb1a4e83f1b82ab19c45f6620ac0c399e52b093
fan <2310403052@qq.com>
2022-11-08 00:07:20 -0800

    列表页分页和排序数据

--- Dmall/apps/goods/urls.py
+++ Dmall/apps/goods/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import (
-   IndexView,
+   IndexView, ListView,
 )
 
 urlpatterns = [
     path('index/', IndexView.as_view()),
+    path('list/<category_id>/skus/', ListView.as_view()),
 ]
