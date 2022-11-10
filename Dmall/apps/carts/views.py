17be716f4551f4523c2fbf5293d1e830c0f9cc33
fan <2310403052@qq.com>
2022-11-10 05:56:24 -0800

    展示购物车接口

--- Dmall/apps/carts/views.py
+++ Dmall/apps/carts/views.py
@@ -93,3 +93,42 @@ class CartsView(View):
             response.set_cookie('carts', carts_base64.decode(), max_age=3600*24)
             #    5.5 返回响应
             return response
+
+
+    def get(self, request):
+        user = request.user
+        if user.is_authenticated:
+            redis_conn = get_redis_connection('carts')
+            sku_id_counts = redis_conn.hgetall('carts_%s' % user.id)
+            selected_ids = redis_conn.smember('selected_%s' % user.id)
+            carts = {}
+
+            for sku_id, count in sku_id_counts:
+                carts[int(sku_id)] = {
+                    'count': int(count),
+                    'selected': sku_id in selected_ids
+                }
+
+        else:
+            cookie_carts = request.COOKIES.get('carts')
+            if cookie_carts:
+                carts = pickle.loads(base64.b64decode(cookie_carts))
+            else:
+                carts = {}
+
+        sku_ids = carts.keys()
+        skus = SKU.objects.filter(id__in=sku_ids)
+
+        sku_list = []
+        for sku in skus:
+            sku_list.append({
+                'id': sku.id,
+                'price': sku.price,
+                'name': sku.name,
+                'default_image_url': sku.default_image.url,
+                'selected': carts[sku.id]['selected'],  # 选中状态
+                'count': int(carts[sku.id]['count']),  # 数量 强制转换一下
+                'amount': sku.price * carts[sku.id]['count']  # 总价格
+            })
+        return JsonResponse({'code': 0, 'errmsg': 'ok', 'cart_skus': sku_list})
+
