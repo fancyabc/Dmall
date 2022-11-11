191adc1de960dd633f54cd2424c9b0e419880502
fan <2310403052@qq.com>
2022-11-10 23:07:33 -0800

    (首页)展示简单购物车

--- Dmall/apps/carts/urls.py
+++ Dmall/apps/carts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import CartsView, CartsSelectAllView
+from .views import CartsView, CartsSelectAllView, CartsSimpleView
 urlpatterns = [
     path('carts/', CartsView.as_view()),
     path('carts/selection/', CartsSelectAllView.as_view()),
+    path('carts/simple/', CartsSimpleView.as_view()),
 ]
