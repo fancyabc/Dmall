785177460e75b896357e8d7da1f85c8fede18f0d
fan <2310403052@qq.com>
2022-11-12 04:42:29 -0800

    使用事务保存订单数据

--- Dmall/apps/orders/views.py
+++ Dmall/apps/orders/views.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from django.views import View
 from django.http import JsonResponse
 from django.utils import timezone
+from django.db import transaction
 
 from django_redis import get_redis_connection
 
@@ -106,54 +107,70 @@ class OrderCommitView(LoginRequiredJSONMixin, View):
         total_amount = Decimal('0')
         freight = Decimal('10.00')
 
-        # 3 数据入库     生成订单（订单基本信息表和订单商品信息表）
-        # 3.1 保存订单基本信息
-        order_info = OrderInfo.objects.create(
-            order_id=order_id,
-            user=user,
-            address=address,
-            total_count=total_count,
-            total_amount=total_amount,
-            freight=freight,
-            pay_method=pay_method,
-            status=status
-        )
+        # 显式的开启一个事务
+        with transaction.atomic():
+            # 创建事务保存点
+            point = transaction.savepoint()
 
-        # 3.2 再保存订单商品信息
-        redis_cli = get_redis_connection('carts')
-        sku_id_counts = redis_cli.hgetall('carts_%s' % user.id)
-        #  获取set
-        selected_ids = redis_cli.smembers('selected_%s' % user.id)
-        carts = {}
+            try:
+                # 3 数据入库     生成订单（订单基本信息表和订单商品信息表）
+                # 3.1 保存订单基本信息
+                order_info = OrderInfo.objects.create(
+                    order_id=order_id,
+                    user=user,
+                    address=address,
+                    total_count=total_count,
+                    total_amount=total_amount,
+                    freight=freight,
+                    pay_method=pay_method,
+                    status=status
+                )
 
-        #  遍历选中商品的id，
-        for sku_id in selected_ids:
-            carts[int(sku_id)] = int(sku_id_counts[sku_id])
+                # 3.2 再保存订单商品信息
+                redis_cli = get_redis_connection('carts')
+                sku_id_counts = redis_cli.hgetall('carts_%s' % user.id)
+                #  获取set
+                selected_ids = redis_cli.smembers('selected_%s' % user.id)
+                carts = {}
 
-        for sku_id, count in carts.items():
+                #  遍历选中商品的id，
+                for sku_id in selected_ids:
+                    carts[int(sku_id)] = int(sku_id_counts[sku_id])
 
-            sku = SKU.objects.get(id=sku_id)
-            # 判断库存是否充足
-            if sku.stock < count:
-                return JsonResponse({'code': 400, 'errmsg': '库存不足'})
-            # 如果充足，则库存减少，销量增加
-            sku.stock -= count
-            sku.sales += count
+                for sku_id, count in carts.items():
 
-            # 累加总数量和总金额
-            order_info.total_count += count
-            order_info.total_amount += (count * sku.price)
+                    sku = SKU.objects.get(id=sku_id)
+                    # 判断库存是否充足
+                    if sku.stock < count:
+                        # 回滚点
+                        transaction.savepoint_rollback(point)
+                        return JsonResponse({'code': 400, 'errmsg': '库存不足'})
+                    # 如果充足，则库存减少，销量增加
+                    sku.stock -= count
+                    sku.sales += count
+                    sku.save()
 
-            # 保存订单商品信息
-            OrderGoods.objects.create(
-                order=order_info,
-                sku=sku,
-                count=count,
-                price=sku.price
-            )
+                    # 累加总数量和总金额
+                    order_info.total_count += count
+                    order_info.total_amount += (count * sku.price)
 
-        # 更新订单的总金额和总数量
-        order_info.save()
+                    # 保存订单商品信息
+                    OrderGoods.objects.create(
+                        order=order_info,
+                        sku=sku,
+                        count=count,
+                        price=sku.price
+                    )
+                # 添加邮费
+                order_info.total_amount += order_info.freight
+                # 更新订单的总金额和总数量
+                order_info.save()
+            except Exception as e:
+                transaction.savepoint_rollback(point)
+                return JsonResponse({'code': 400, 'errmsg': '下单失败'})
+            # 提交点
+            transaction.savepoint_commit(point)
+
 
         # 4 将redis中选中的商品信息移除出去 （暂缓）
         return JsonResponse({'code': 0, 'errmsg': 'ok', 'order_id': order_id})
