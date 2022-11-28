1d1858bc68afbc2d833c8efa61ac7e1cd7e27c4b
fan <2310403052@qq.com>
2022-11-27 23:09:52 -0800

    月增用户统计

--- Dmall/apps/dmall_admin/urls.py
+++ Dmall/apps/dmall_admin/urls.py
@@ -3,12 +3,14 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.urls import path
 
 from .views.admintokenview import dmall_token
-from .views.statics import DailyActiveAPIView, DailyOrderCountAPIView
+from .views.statics import DailyActiveAPIView, DailyOrderCountAPIView, MonthCountAPIView
 
 urlpatterns = [
     path('authorizations/', dmall_token),
 
     path('statistical/day_active/', DailyActiveAPIView.as_view()),  # 日活统计
     path('statistical/day_orders/', DailyOrderCountAPIView.as_view()),
+
+    path('statistical/month_increment/', MonthCountAPIView.as_view()),
 ]
 
