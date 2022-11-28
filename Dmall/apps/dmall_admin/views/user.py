025fc74624be2d99baa2e225b12546001db61eef
fan <2310403052@qq.com>
2022-11-28 02:48:56 -0800

    用户信息展示(搜索的实现--重写 get_queryset方法)

--- Dmall/apps/dmall_admin/views/user.py
+++ Dmall/apps/dmall_admin/views/user.py
@@ -7,6 +7,11 @@
             1.2 将对象列表转换为 满足需求的字典列表 (序列化器)
             1.3 返回响应
         2. 再实现搜索功能
+                es - elasticsearch
+                模糊查询
+
+                获取 keyword
+                根据 keywork 进行 模糊查询
         3. 最后实现分页
 """
 from collections import OrderedDict
@@ -39,8 +44,12 @@ class PageNum(PageNumberPagination):
 
 
 class UserAPIView(ListAPIView):
-
-    queryset = User.objects.all()
+    # 设置方法  def get_queryset(self): 根据  不同的业务逻辑返回不同的查询结果集
+    def get_queryset(self):
+        keyword = self.request.query_params.get('keyword')
+        if keyword:
+            return User.objects.filter(username__contains=keyword)
+        return User.objects.all()
 
     serializer_class = UserModelSerializer
 
