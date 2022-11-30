b4b5941d06906b0f3a096ab1702ecc3af9ca574d
fan <2310403052@qq.com>
2022-11-29 23:57:29 -0800

    sku新增的第3个问题:specs要定义字段

--- Dmall/apps/dmall_admin/serializers/sku.py
+++ Dmall/apps/dmall_admin/serializers/sku.py
@@ -1,7 +1,17 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 
-from goods.models import SKU, GoodsCategory, SPU, SPUSpecification, SpecificationOption
+from goods.models import SKU, GoodsCategory, SPU, SPUSpecification, SpecificationOption, SKUSpecification
+
+
+# SKUSpecification 序列化器 -- sku规格和规格选项
+class SKUSpecificationModelSerializer(ModelSerializer):
+    spec_id = serializers.IntegerField()
+    option_id = serializers.IntegerField()
+
+    class Meta:
+        model = SKUSpecification
+        fields = ['spec_id', 'option_id']
 
 
 class SKUModelSerializer(ModelSerializer):
@@ -34,6 +44,15 @@ class SKUModelSerializer(ModelSerializer):
         新的疑问!!!  默认的字段是 required=True
 
         StringRelatedField 本质是 获取 关联模型的 __str__ 里的数据
+
+    Q3. 当我们的sku保存了之后, sku的最终 (规格)选项 也应该入库
+        specs: [{spec_id: "4", option_id: 8}, {spec_id: "5", option_id: 11}] 这个没有入库!!!!
+
+        # validated_data  其实是等于 我们序列化器 通过字段 验证的数据
+        # 当前 我们在 序列化器中 并没有定义 specs 所以 validated_data 里没有 specs
+        # 我们想要获取 specs 有至少2种方法
+        # 1. 添加 specs 字段
+        # 2. 通过 断点我们发现  self.context['request'] 可以获取 当前的请求对象.
     """
     spu_id = serializers.IntegerField()
     category_id = serializers.IntegerField()
@@ -41,9 +60,19 @@ class SKUModelSerializer(ModelSerializer):
     spu = serializers.StringRelatedField(required=False)
     category = serializers.StringRelatedField()
 
+    specs = SKUSpecificationModelSerializer(many=True)
+
     class Meta:
         model = SKU
         fields = '__all__'
+
+    def create(self, validated_data):
+        # validated_data  其实是等于 我们序列化器 通过字段 验证的数据
+        # 当前 我们在 序列化器中 并没有定义 specs 所以 validated_data 里没有 specs
+        # 我们想要获取 specs 有至少2种方法
+        # 1. 添加 specs 字段
+        # 2. 通过 断点我们发现  self.context['request'] 可以获取 当前的请求对象.
+        pass
 
 
 class GoodsCategoryModelSerializer(ModelSerializer):
